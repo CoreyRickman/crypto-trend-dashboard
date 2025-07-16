a4ff743a2a2175674b28c7dc74c06974d057e506
CoreyRickman <cmrickman15@gmail.com>
2025-07-16 16:05:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,6 @@ def train_model(df):
         learning_rate=0.05,
         subsample=0.8,
         colsample_bytree=0.8,
-        use_label_encoder=False,
         eval_metric="logloss",
         random_state=42
     )
